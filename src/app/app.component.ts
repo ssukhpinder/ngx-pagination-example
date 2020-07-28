d3ff8436df06ee4c53f9824fd04801609ccaab1b
Sukhpinder Singh <sukhpinder.singh@sourcefuse.com>
2020-07-28 21:43:13 +0530

    Initial Chekin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-pagination-example';
+  config: any;
+  collection = { count: 30, data: [] };
+
+  configCustomPagination: any;
+  collectionCustomPagination = { count: 30, data: [] };
+  constructor() {
+    for (var i = 0; i < this.collection.count; i++) {
+      this.collection.data.push(
+        {
+          id: i + 1,
+          value: "Collection value " + (i + 1)
+        }
+      );
+    }
+    this.collectionCustomPagination = this.collection;
+
+    this.config = {
+      id: 'basicPaginate',
+      itemsPerPage: 5,
+      currentPage: 1,
+      totalItems: this.collection.count
+    };
+    this.configCustomPagination = {
+      id: 'customPaginate',
+      itemsPerPage: 5,
+      currentPage: 1,
+      totalItems: this.collectionCustomPagination.count
+    };
+  }
+
+  public maxSize: number = 7;
+  public directionLinks: boolean = true;
+  public autoHide: boolean = false;
+  public responsive: boolean = true;
+  public labels: any = {
+    previousLabel: '<--',
+    nextLabel: '-->',
+    screenReaderPaginationLabel: 'Pagination',
+    screenReaderPageLabel: 'page',
+    screenReaderCurrentLabel: `You're on page`
+  };
+
+  pageChanged(event) {
+    this.config.currentPage = event;
+  }
+
+  onPageChange(event) {
+    this.configCustomPagination.currentPage = event;
+  }
 }
